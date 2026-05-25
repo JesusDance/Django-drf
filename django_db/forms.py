91b555eaafc28a99a015a3e847a0bf43f2832384
Jesus <jiesusdance@gmail.com>
2026-05-25 23:10:59 +0300

    refactor forms.py

--- django_db/forms.py
+++ django_db/forms.py
@@ -1,16 +1,11 @@
-from django.forms import ModelForm, DateInput
-from .models import Client, Game
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms import ModelForm, DateInput
+
+from .models import Client, Game
 
 
 class UserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ["username", "email"]
-
-
-class UserForm2(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email"]
@@ -20,13 +15,14 @@ class ClientForm(ModelForm):
     class Meta:
         model = Client
         fields = "__all__"
+        exclude = ["user"]
         widgets = {'birthday': DateInput(attrs={'type': 'date'})}
 
 
 class GameForm(ModelForm):
     class Meta:
         model = Game
-        fields = "__all__"
+        fields = ["name", "genre", "description", "wiki_page"]
 
 
 
