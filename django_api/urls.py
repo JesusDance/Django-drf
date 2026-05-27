b01c4744537e50ddfd280828554f8b63cc4b45c9
Jesus <jiesusdance@gmail.com>
2026-05-27 10:11:16 +0300

    refactor urls.py for tests

--- django_api/urls.py
+++ django_api/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
-from .views import CreateUser, GameViewSet, GameListViewSet, user_login
 from rest_framework.routers import SimpleRouter
 
+from .views import CreateUser, GameViewSet, user_login
 
 router = SimpleRouter()
 router.register(r'games', GameViewSet, 'games')
-router.register(r'games-list', GameListViewSet, 'games-list')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include((router.urls, 'rest_api'), namespace='rest_api')),
     path('create-user/', CreateUser.as_view(), name='create-user'),
     path('login/', user_login, name='login'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
