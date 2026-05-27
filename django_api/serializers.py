38161bbd9a012f46f8834e2c80ff79c57b76d238
Jesus <jiesusdance@gmail.com>
2026-05-27 10:10:26 +0300

    refactor serializers

--- django_api/serializers.py
+++ django_api/serializers.py
@@ -1,6 +1,7 @@
-from rest_framework import serializers
-from django_db.models import Client, Game, GameList
 from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from django_db.models import Game
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,6 +10,10 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            return user
+
         # def create(self, validated_data):
         #     password = validated_data.pop("password")
         #     user = User(**validated_data)
@@ -16,29 +21,16 @@ class UserSerializer(serializers.ModelSerializer):
         #     user.save()
         #     return user
 
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            return user
 
-
-class ClientModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Client
-        fields = "__all__"
+# class ClientModelSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Client
+#         fields = "__all__"
 
 
 class GameModelSerializer(serializers.ModelSerializer):
+    genre = serializers.CharField(source="get_genre_display")
+
     class Meta:
         model = Game
-        fields = "__all__"
-
-
-class GameListSerializer(serializers.ModelSerializer):
-    gamer = serializers.CharField(source="gamer.username", read_only=True)
-    game_list = GameModelSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = GameList
-        fields = "__all__"
-
-
+        fields = ["id", "name", "genre", "description", "wiki_page"]
